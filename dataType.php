9d6b77e75e19867cfc76dc07e57dc6cdf91d06f7
Mohammad Akour <pro.akour@gmail.com>
2023-12-31 19:09:31 +0300

    Create a new file

---
+++ dataType.php
@@ -0,0 +1,70 @@
+<?php
+
+// $id = intval($_GET['id']);
+
+// $int = -10;
+// intval()
+// var_dump($int);
+
+// $float = 10.3;
+// floatval()
+// var_dump($float);
+
+// $name = "Mohammad";
+// strval()
+// var_dump($name);
+
+// $test = NULL;
+// var_dump($test);
+
+// $its_cat = FALSE;
+// var_dump($its_cat);
+
+
+// $omar = "Omar";
+// echo "My name is $omar";
+// echo 'My name is $omar';
+
+
+// $text = 32710327139;
+// echo strlen($text);
+
+// $cat = "this is some text.";
+// echo str_word_count($cat);
+
+// $test = "My name is Mohammad";
+// echo strpos($test, "is");
+
+
+// * STRing TO Upper
+// $upper = "abdullah";
+// echo strtoupper($upper);
+
+// * STRing TO Lower
+// $lower = "ABDULLAH";
+// echo strtolower($lower);
+
+
+// $country = "I Love Jordan";
+// echo str_replace("Jordan", "Egypt", $country);
+// $url = "jordan is here";
+// echo str_replace(" ", "-", $url);
+
+// $words = "My name";
+// echo strrev($words);
+
+// $ahmad = "      Hello   Ahmad    ";
+// echo trim($ahmad);
+
+
+// $fullname = "Mohammad Akour";
+// echo substr($fullname, 2, 5);
+
+
+// echo "Welcome mr. Ahmad  ";
+// echo 'Welcome mr. Ahmad  ';
+
+
+// $number = 123;
+// var_dump(is_int($number));
+// var_dump(is_float($number));
