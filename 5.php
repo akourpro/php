df6a61999f6059bacfbe6db3b6091ecb8b572dbf
Mohammad Akour <pro.akour@gmail.com>
2024-01-02 19:14:22 +0300

    المحاضرة الخامسة

---
+++ 5.php
@@ -0,0 +1,75 @@
+<?php
+
+// $num = 25; // int
+
+// $num = (string) $num;
+
+// var_dump($num);
+
+
+// $number_one = 10;
+// $number_two = 2;
+
+
+// echo $number_one ** $number_two;
+
+// $y = 1;
+// $a = 14;
+
+// $y += $a;
+
+// $y = $y + $a;
+
+// echo $y;
+
+
+/**
+ * 
+ * = نفس نوع قيمة البيانات
+ * == مطابق لنفس النوع ونفس القيمة
+ * === true , false
+ * 
+ *  */
+
+
+// echo pi();
+
+// جلب اكبر/اصغر قيمة
+// echo min(1, 5, 10, 100, 1000, -10, -11);
+// echo max(1, 5, 10, 100, 1000, -10, -11);
+
+// تحويل القيم من سالبة الى موجبة
+// echo abs(-100);
+
+// الجذر التربيعي
+// echo sqrt(100);
+
+// الجبر
+// echo round(0.49);
+// echo round(0.61);
+
+
+// طباعة ارقام عشوائية
+// echo rand(1, 10);
+
+
+
+// define("PATH", "test", true);
+// echo essa;
+// echo path;
+
+// const AHMAD = "Welcome to W3Schools.com";
+// echo AHMAD;
+
+
+// echo __DIR__;
+// echo __FILE__;
+// echo __LINE__;
+
+
+/**
+ * انشئ نموذج يطلب من المستخدم ادخال اسمه
+ * ثم قم بالتحقق من أن القيمة المدخلة نصية
+ * ثم احفظ الاسم داخل define اسمه NAME
+ * ثم اطبع قيمة الـ define
+ */
